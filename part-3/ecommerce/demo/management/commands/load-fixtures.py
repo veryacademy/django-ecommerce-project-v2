f957660012666076b6bcf089d26eeb2b08e3ffdc
veryacademy <veryacademy@gmail.com>
2021-11-17 21:23:10 +0000

    nc

--- part-3/ecommerce/demo/management/commands/load-fixtures.py
+++ part-3/ecommerce/demo/management/commands/load-fixtures.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
         call_command("loaddata", "db_admin_fixture.json")
         call_command("loaddata", "db_category_fixture.json")
         call_command("loaddata", "db_product_fixture.json")
-        call_command("loaddata", "db_product_category.json")
+        call_command("loaddata", "db_category_product_fixture.json")
         call_command("loaddata", "db_type_fixture.json")
         call_command("loaddata", "db_brand_fixture.json")
         call_command("loaddata", "db_product_inventory_fixture.json")
